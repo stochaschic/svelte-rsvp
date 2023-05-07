90f539263f681210a2c5af14992f53c27d297366
Rizka Azmira <cika@Rizkas-MacBook-Air.local>
2023-05-07 11:14:43 +0700

    Initial commit

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,6 @@
+<form name="will-attend" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+  <input type="hidden" name="form-name" value="will-attend" />
+  <label for="name">Name</label>
+  <input name="name" id="name" required placeholder="Name" type="text" />
+  <input type="submit" value="Submit" />
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script>
+  import '../app.css';
+  import Modal from '$lib/Modal.svelte';
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+Modal:
+<Modal/>
+Index:
+<form name="will-attend" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+  <input type="hidden" name="form-name" value="will-attend" />
+  <label for="name">Name</label>
+  <input name="name" id="name" required placeholder="Name" type="text" />
+  <input type="submit" value="Submit" />
+</form>
+No netlify:
+<form>
+  <input type="hidden" name="form-name" value="will-attend" />
+  <label for="name">Name</label>
+  <input name="name" id="name" required placeholder="Name" type="text" />
+  <input type="submit" value="Submit" />
+</form>
